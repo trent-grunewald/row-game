3632cc0e2f41a77d1a5220d96f11335659ab3ac0
Trent <39209726+trent-grunewald@users.noreply.github.com>
2018-11-27 22:43:16 -0800

    delete Player.js

--- js/Player.js
+++
@@ -1,9 +0,0 @@
-class Player {
-    constructor(name, id, color, active = false){
-        this.name = name;
-        this.id = id;
-        this.color = color;
-        this.active = active;
-        this.tokens = []
-    }
-}
