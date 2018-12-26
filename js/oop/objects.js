6cbd7bb00fdafbfe3cdf8262bf42d91275b46886
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-12-26 12:32:48 -0800

    setters

--- js/oop/objects.js
+++ js/oop/objects.js
@@ -19,12 +19,12 @@ class Pet {
     return 'sleeping';
   }
 
-//Use this to return the value of the set Owner
+//Use this to return the value of the set Owner, don't forget the _
 get owner(){
   return this._owner;
 }
 
-  //setter names CANNOT be the same as the property
+  //setter names CANNOT be the same as the property, notice the _ before the "this._owner".
   set owner(owner) {
    this._owner = owner;
     
@@ -59,3 +59,4 @@ suze.owner = new Owner('Trent', '4 Nth 62nd ave');
 suze.owner.phone = '(555) 555-5555';
 
 console.log(suze.owner);
+
