acca28762a20779a86e0581d077bdd9ead96fbb2
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-12-27 21:57:00 -0800

    reduce practice

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -61,7 +61,7 @@ let lowercaseNames = [];
 // const no3 = numbers.filter(number => number !==3);
 // console.log(no3);
 
-// ---- Returns only event numbers
+// ---- Returns only even numbers
 // const no3 = numbers.filter(number => number % 2 === 0);
 // console.log(no3);
 
@@ -137,15 +137,70 @@ let lowercaseNames = [];
 //   console.log(gNames);
 
 
-    const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
-let numberOf503 = phoneNumbers.reduce((count, number) => {
-  if(number[1] === "5") {
-    return count +1;
-  }
-  return count;
-}, 0);
+//     const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
+// let numberOf503 = phoneNumbers.reduce((count, number) => {
+//   if(number[1] === "5") {
+//     return count +1;
+//   }
+//   return count;
+// }, 0);
 
-console.log(numberOf503)
+// console.log(numberOf503)
 
-// numberOf503 should be: 3
+
+
+
+// -----------------------------------------ITERATION METHODS-------------------------
+
+// //Adds the Filter and Map methods to return the 21st century years with A.D.
+// const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+// let displayYears;
+
+// displayYears = years
+// .filter(year => year >= 2001)
+// .map(year => year + " A.D.");
+
+// console.log(displayYears);
+
+
+//Filters an array of users based on Name
+const users = [
+  {name: 'Samir', age: 27},
+  {name: 'Angela', age: 33},
+  {name: 'Beatrice', age: 42}
+];
+let names;
+// names = users.filter(user => user.name !== 'Samir');
+// names = users.map(user => `${user.name} is ${user.age} years old.`);
+// console.log(names);
+
+
+//return usersname and Age as object
+
+/* create a new variable called shortUsers
+    userObject is the accumulator
+    userObject is initialized as an object (curly braces at end)
+    user is the individual entry in the users array that is looped through
+    userObject[user.name] is creating a new object key for each name
+    user.age is the value being assigned to the new object key each time
+    return userObject puts each object entry (each time through the loop) into shortUsers*/
+// const shortUsers = users.reduce((userObject, user) => {
+//   userObject[user.name] = user.age;
+//   return userObject
+// }, {}); //The curly braces are the initial value of the object
+
+// console.log(shortUsers);
+
+
+const authors = [
+  { firstName: "Beatrix", lastName: "Potter" },
+  { firstName: "Ann", lastName: "Martin" },
+  { firstName: "Beverly", lastName: "Cleary" },
+  { firstName: "Roald", lastName: "Dahl" },
+  { firstName: "Lewis", lastName: "Carroll" }
+];
+let fullAuthorNames = authors.map( user => `${user.firstName} ${user.lastName}`);
+console.log(fullAuthorNames);
+
+// fullAuthorNames should be: ["Beatrix Potter", "Ann Martin", "Beverly Cleary", "Roald Dahl", "Lewis Carroll"]
 // Write your code below
