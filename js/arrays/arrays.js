1f4fa3aac893cd30f08349a6b2188dfbdb50c83d
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-12-30 17:11:29 -0800

    reduce practice

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -375,3 +375,34 @@ let lowercaseNames = [];
 // console.log(flattenedCustomerNames)
 
 
+const customers = [
+  {
+    name: "Tyrone",
+    personal: {
+      age: 33,
+      hobbies: ["Bicycling", "Camping"]
+    }
+  },
+  {
+    name: "Elizabeth",
+    personal: {
+      age: 25,
+      hobbies: ["Guitar", "Reading", "Gardening"]
+    }
+  },
+  {
+    name: "Penny",
+    personal: {
+      age: 36,
+      hobbies: ["Comics", "Chess", "Legos"]
+    }
+  }
+];
+let hobbies = customers.reduce((arr, hobbies) => [...arr,...hobbies.personal.hobbies],[]);
+
+console.log(hobbies)
+
+// hobbies should be: ["Bicycling", "Camping", "Guitar", "Reading", "Gardening", "Comics", "Chess", "Legos"]
+// Write your code below
+
+
