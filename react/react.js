92ac65622f671aa6a611ff2df33a50104ee1a1c4
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2019-01-03 09:09:35 -0800

    React Props

--- react/react.js
+++ react/react.js
@@ -24,8 +24,31 @@
 
 // Creating reusable COMPONENTS
 
+const players = [
+  {
+    name: "Trent",
+    score: 77,
+    id: 1
+  },
+  {
+    name: "Travis",
+    score: 3,
+    id: 2
+  },
+  {
+    name: "John",
+    score: 12,
+    id: 3
+  },
+  {
+    name: "Joe",
+    score: 31,
+    id: 4
+  }
+];
+
 //Capitalize the function name to specify a React component.
-//props is the header array / arguement - title and totalPlayers are set in the app Component below
+//props is the header array / arguement - title and totalPlayers are set in the app Component below and can be called whatever you want
 const Header = (props) => {
   return (
     <header>
@@ -35,33 +58,44 @@ const Header = (props) => {
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        Trent
+        { props.name }
       </span>
       {/* A component containing another component is called "Composition" */}
-      <Counter />      
+      <Counter score = { props.score }/>  
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">35</span>
+        <span className="counter-score"> { props.score } </span>
         <button className="counter-action increment"> + </button>
       </div>
   );
 }
 
-const App = () => {
+
+const App = (props) => {
   return (
     <div className="scoreboard">
-    <Header title="Scoreboard" totalPlayers={1} />
-    <Player />
+    <Header 
+    title="Scoreboard" 
+    totalPlayers={ props.initialPlayers.length } 
+    />
+    {/* This returns the value of the "initialPlayers" array and creates each player */}
+    { props.initialPlayers.map( player =>
+      <Player
+      name={ player.name }
+      score={ player.score }
+      key={ player.id.toString() }
+      />
+      )}
     </div>
   );
 }
@@ -69,6 +103,6 @@ const App = () => {
 ReactDOM.render(
   //CAPITALIZED to refer to a React component and not an HTML Element.
   //Can be self closing if you don't have children.
-  <App />,
+  <App initialPlayers={ players }/>,
   document.getElementById('root')
 )
