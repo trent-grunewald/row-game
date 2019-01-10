9197b3af3c8af0adaab1af64a2e2a5e2bb65978a
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2019-01-09 22:28:10 -0800

    Weather App

---
+++ react/sg-tut/weather/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div>Buy the barn</div>;
+};
+
+export default SeasonDisplay;
---
+++ react/sg-tut/weather/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+  return <SeasonDisplay />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
