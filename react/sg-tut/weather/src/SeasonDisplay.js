299c680a503e0e799808209787a1a9469821a7c8
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2019-01-12 23:54:21 -0800

    state

--- react/sg-tut/weather/src/SeasonDisplay.js
+++ react/sg-tut/weather/src/SeasonDisplay.js
@@ -24,14 +24,19 @@ class SeasonDisplay extends React.Component {
   }
 
   render() {
-    return (
-      <h1>
-        Buy The Barn: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </h1>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <h1>Buy The Barn: {this.state.lat}</h1>;
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <h1>BUY THE BARN!, but first, fix your code: {this.err.message}</h1>
+      );
+    }
+
+    if (!this.state.err && !this.state.lat) {
+      return;
+    }
   }
 }
-
 export default SeasonDisplay;
--- react/sg-tut/weather/src/index.js
+++ react/sg-tut/weather/src/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return <SeasonDisplay />;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <h1>Buy The Barn: {this.state.errorMessage}</h1>;
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <h1>BUY THE BARN! {this.state.lat}</h1>;
+    }
+
+    if (!this.state.errorMessage && !this.state.lat) {
+      return <div />;
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
