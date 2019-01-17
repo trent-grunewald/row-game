38b1cfcd3871488e23562332252a49fe2f3f3aaa
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2019-01-16 18:27:11 -0800

    FINAL

--- react/sg-tut/weather/src/index.js
+++ react/sg-tut/weather/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from "react-dom";
 import Loader from "./Loader";
 import SeasonDisplay from "./SeasonDisplay";
 
+// class UserForm extends React.Component {
+//   render() {
+//     return(
+//     <form>
+//       <label>Enter a username: </label>
+//       <input type="text"/>
+//     </form>
+//     )
+//   }
+// }
+
+// class Clock extends React.Component {
+//   state = { time: new Date().toLocaleTimeString() };
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState({ time: new Date().toLocaleTimeString() });
+//     }, 1000);
+//   }
+
+//   render() {
+//     return <div className="time">The time is: {this.state.time}</div>;
+//   }
+// }
+
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
 
